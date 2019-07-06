bdff6e80360f3441754bbae0387820723d61068e
james <hamishwatson@live.com>
2019-07-07 02:13:22 +0800

    auth

--- src/helper/permission.js
+++ src/helper/permission.js
@@ -2,20 +2,24 @@ import md5 from 'md5';
 import * as di from '@/DAL';
 import {URI, encodeBase64} from './index';
 
+const AUTH_ADDR = 'https://wiki.watsonserve.com/api/auth.html';
+
 export function checkEditable() {
   const foo = (~~(Math.random() * 10000000)).toString(16);
+  let salt = '';
   return encodeBase64(foo)
-  .then(salt => {
-    return di.getUserInfo(salt)
-    .then(userInfo => {
-      if (userInfo.token) {
-        const {token, app} = userInfo;
-        const redirect = window.location.href;
-        const signal = md5(app + token + salt + redirect);
-        const params = URI.encode({app, token, salt, redirect, signal});
-        window.location.href = `https://passport.cn-bar.com/?${params}`;
-      }
+  .then(_salt => {
+    salt = _salt;
+    return di.getUserInfo(salt);
+  })
+  .then(userInfo => {
+    if (!userInfo.token) {
       return userInfo;
-    })
+    }
+    const {token, app} = userInfo;
+    const redirect = `${AUTH_ADDR}?rd=${encodeURIComponent(window.location.pathname)}`;
+    const signal = md5(app + token + salt + redirect);
+    const params = URI.encode({app, token, salt, redirect, signal});
+    window.location.href = `https://passport.watsonserve.com/?${params}`;
   });
 }
